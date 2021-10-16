c78c1c58b8e665e8fa2083f59018af5db62ad89c
aburto22 <aburto22@gmail.com>
2021-10-16 18:59:43 +0200

    Added tailwind, eslint and prettier

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App bg-gray-200 h-100 w-screen h-screen">
+      <p>I am the app</p>
+    </div>
+  );
+}
+
+export default App;
